2eb422312d9a7c1caea7ecaed8b21f55f9dda750
zaali <zaali@live.com>
2019-04-05 21:06:23 +0400

    Add jwt

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require("express");
 const liveQuery = require("@graphile/subscriptions-lds")
 const { postgraphile, makePluginHook } = require("postgraphile");
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
+const ConnectionFilterPlugin = require("postgraphile-plugin-connection-filter");
 const subscriptionPlugin = require("./subscriptionPlugin");
 const { default: PgPubsub } = require("@graphile/pg-pubsub");
+const jwt = require("jsonwebtoken");
 const pluginHook = makePluginHook([PgPubsub]);
 
 const app = express();
 const options = {
+  pgSettings: req => {
+    getClaims(req)
+  },
   pluginHook,
   live: true,
+  defaultRole: "kodala",
   ownerConnectionString: 'postgres://zaali:postgres@localhost:5432/kodala_dev',
   // subscriptions: true,
   appendPlugins: [
     PgSimplifyInflectorPlugin,
-    liveQuery.default
+    liveQuery.default,
+    ConnectionFilterPlugin
     // subscriptionPlugin
   ],
   graphileBuildOptions: {
@@ -24,7 +31,8 @@ const options = {
   watchPg: true,
   simpleCollections: "only",
   ignoreRBAC: false,
-  jwtPgTypeIdentifier: 'private.jwt_token',
+  jwtPgTypeIdentifier: 'api.jwt_token',
+  jwtToken: 'api.jwt_token',
   disableQueryLog: true,
   jwtSecret: "mySecret"
 }
@@ -32,5 +40,38 @@ const options = {
 app.use(
   postgraphile('postgres://kodala:postgres@localhost:5432/kodala_dev', ["api"], options)
 );
+
+const getClaims = (req) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new Error("No authorization header provided.");
+    }
+
+    const authSplit = authHeader.split(" ");
+    if (authSplit.length !== 2) {
+      throw new Error(
+        'Malformed authentication header. "Bearer accessToken" syntax expected.'
+      );
+    } else if (authSplit[0].toLowerCase() !== "bearer") {
+      throw new Error(
+        '"Bearer" keyword missing from front of authorization header.'
+      );
+    }
+
+    const token = authSplit[1];
+    const decodedToken = jwt.decode(token);
+    if (decodedToken === null) {
+      throw new Error("Unable to decode JWT, refresh login and try again.");
+    }
+    return {
+      'jwt.claims.user_id': decodedToken.user_id,
+    }
+  } catch (e) {
+    e.status = 401; // append a generic 401 Unauthorized header status
+    throw e;
+  }
+}
+
 
 app.listen(process.env.PORT || 5000);
